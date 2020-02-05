1fec9b425fc2cafe2fa32639714276da78f41a6a
viktor <viktoras.deni@gmail.com>
2020-02-05 20:03:39 +0200

    new route for charts/stats

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -2,10 +2,34 @@ const express = require("express");
 const route = express.Router();
 const adminAuth = require('../../middlewares/adminAuth');
 
+
 route.get("/", adminAuth, (req, res) => {
+
     res.json({
         success: true,
         message: 'Admin area'
+    });
+});
+route.get("/stats", adminAuth, async (req, res) => {
+    const categories = await Category.find({}).exec();
+    const labels = categories.map(c => c.title);
+    const counts = [];
+    for(let cat of categories) {
+      const num =  await  Product.count({ category: cat._id});
+      counts.push(num);
+    }
+
+    res.json({
+        success: true,
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: "My Categories",
+                    data: counts
+                }
+            ]
+        }
     });
 });
 
